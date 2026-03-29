d52718567024baa50179e36c6aa7af0411f880dd
Adrian Tan <adriantanjm@gmail.com>
2026-03-29 17:10:25 +0100

    Build site for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://adriantanjm-13.github.io',
+  base: '/adrian-tan-portfolio',
   root: '.',
   outDir: './dist',
   publicDir: './public',
